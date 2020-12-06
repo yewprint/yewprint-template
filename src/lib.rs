9fa83ed603a2268c7953286dfdc6e2adc3f9af93
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-12-06 17:47:56 +0100

    Add missing allocator to the code

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@
 
 use wasm_bindgen::prelude::*;
 
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
